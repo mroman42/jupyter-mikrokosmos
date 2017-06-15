1cbebbebd410346d4cbdafa7699b8ece5598a4aa
M42 <mromang08@gmail.com>
2017-06-15 12:46:45 +0200

    create mikrokosmos kernel
    using pexpect and decode('utf8')

---
+++ mikrokosmoskernel.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from ipykernel.kernelbase import Kernel
+import pexpect
+
+class MikrokosmosKernel(Kernel):
+    implementation = 'IMikrokosmos'
+    implementation_version = '0.1'
+    language = 'mikrokosmos'
+    language_version = '0.2'
+    language_info = {'mimetype': 'text/plain'}
+    banner = "Mikrokosmos - A lambda calculus interpreter"
+
+    # Initialization
+    mikro = pexpect.spawn('mikrokosmos')
+    mikro.expect('mikro>')
+    
+    def do_execute(self, code, silent,
+                   store_history=True,    # Whether to record code in history
+                   user_expressions=None,
+                   allow_stdin=False):
+
+        # Send code to mikrokosmos
+        self.mikro.sendline(code)
+        self.mikro.expect('mikro>')
+        output = self.mikro.before.decode('utf8')
+        
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=MikrokosmosKernel)
