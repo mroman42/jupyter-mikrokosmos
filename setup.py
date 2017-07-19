bc298b85e9360b48874a1cbca0fd39c914c0e669
M42 <mromang08@gmail.com>
2017-07-19 20:57:19 +0200

    pip setup
    Closes #2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='imikrokosmos',
+    version='0.1.2',
+    url='https://github.com/M42/jupyter-mikrokosmos',
+    author='Mario Román',
+    author_email='mromang08@gmail.com',
+    license='GPLv3'
+    )
