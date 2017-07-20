370bdf9fbd8a51411c78e29671e418e8d87049d0
M42 <mromang08@gmail.com>
2017-07-20 19:15:49 +0200

    Modify setup to work with pip

--- setup.py
+++ setup.py
@@ -7,12 +7,27 @@ from os.path import dirname,abspath
 from shutil import copy as file_copy
 from setuptools import setup, find_packages
 
+# Post installation of the Jupyter kernelspec
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        install_my_kernel_spec()
+        develop.run(self)
+        
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install_my_kernel_spec()
+        install.run(self)
 
 # This script to easily install the kernel spec has been taken from
 # the Sebastian Gutsche implementation of the Singular Jupyter kernel,
 # which can be read here
 #  https://github.com/sebasguts/jupyter_kernel_singular
-def install_my_kernel_spec(user=True):
+def install_my_kernel_spec(user=False):
     with TemporaryDirectory() as td:
         os.chmod(td, 0o755) # Starts off as 700, not user readable
         path_of_file = dirname( abspath(__file__) )
@@ -36,4 +51,8 @@ setup( name='imikrokosmos'
      , author_email='mromang08@gmail.com'
      , license='GPLv3'
      , packages=['mikrokosmoskernel']
+     , cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+       }
      )
