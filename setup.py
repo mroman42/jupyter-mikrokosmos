f8baeba9d34a68c3b893a2bcb8a122fc19e53cff
M42 <mromang08@gmail.com>
2017-07-20 18:03:54 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,39 @@
-from setuptools import setup
+#!/usr/bin/env python
+import sys
+import os
+from jupyter_client.kernelspec import install_kernel_spec
+from IPython.utils.tempdir import TemporaryDirectory
+from os.path import dirname,abspath
+from shutil import copy as file_copy
+from setuptools import setup, find_packages
 
-setup(
-    name='imikrokosmos',
-    version='0.1.2',
-    url='https://github.com/M42/jupyter-mikrokosmos',
-    author='Mario Román',
-    author_email='mromang08@gmail.com',
-    license='GPLv3'
-    )
+
+# This script to easily install the kernel spec has been taken from
+# the Sebastian Gutsche implementation of the Singular Jupyter kernel,
+# which can be read here
+#  https://github.com/sebasguts/jupyter_kernel_singular
+def install_my_kernel_spec(user=True):
+    with TemporaryDirectory() as td:
+        os.chmod(td, 0o755) # Starts off as 700, not user readable
+        path_of_file = dirname( abspath(__file__) )
+        file_copy(path_of_file + "/kernel.js", td)
+        file_copy(path_of_file + "/kernel.json", td)
+        print('Installing Jupyter kernel spec')
+        install_kernel_spec(td, 'IMikrokosmos', user=user, replace=True)
+
+def main(argv=None):
+    install_my_kernel_spec()
+
+if __name__ == '__main__':
+    main()
+    
+# pip setup
+setup( name='imikrokosmos'
+     , description='A Jupyter kernel for the mikrokosmos lambda interpreter'
+     , version='0.1.2'
+     , url='https://github.com/M42/jupyter-mikrokosmos'
+     , author='Mario Román'
+     , author_email='mromang08@gmail.com'
+     , license='GPLv3'
+     , packages=['mikrokosmoskernel']
+     )
